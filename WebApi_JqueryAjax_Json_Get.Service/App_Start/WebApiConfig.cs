f07e393df8e346486a91ac0d107a726c267482bd
ibavli <ibrahim.bavli35@gmail.com>
2018-08-11 02:41:34 +0300

    jquery ajax kodlarını yazdım. Service katmanında nugetten Microsoft.AspNet.WebApi.Cors indirdim. WebApiConfig içerisinde dışarı açma ve json veri döndürme kodlarını yazdım. Yorum satırlarla mevcut.

--- WebApi_JqueryAjax_Json_Get.Service/App_Start/WebApiConfig.cs
+++ WebApi_JqueryAjax_Json_Get.Service/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace WebApi_JqueryAjax_Json_Get.Service
 {
@@ -9,9 +10,10 @@ namespace WebApi_JqueryAjax_Json_Get.Service
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
+            //apiyi dışarı açtık. Bunun için nugetten Microsoft.AspNet.WebApi.Cors yükledik.
+            var corsAttr = new EnableCorsAttribute("http://localhost:13592", "*", "*");
+            config.EnableCors(corsAttr);
 
-            // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
@@ -19,6 +21,9 @@ namespace WebApi_JqueryAjax_Json_Get.Service
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            //veriler json dönmesi için.
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
         }
     }
 }
